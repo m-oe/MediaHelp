125d185d42002e6f717032c1f2107b1eed3f5261
fedi.abid <fedi.abid@gmail.com>
2017-08-22 17:43:59 +0200

    routing + Hauptseite funktionnieren

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 
+import { HauptSeiteComponent } from './haupt-seite/haupt-seite.component';
+
+const appRoutes: Routes = [
+  { path: 'hauptseite', component: HauptSeiteComponent },
+  {
+    path: '',
+    redirectTo: '/hauptseite',
+    pathMatch: 'full'
+  },
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HauptSeiteComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
---
+++ src/app/haupt-seite/haupt-seite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-haupt-seite',
+  templateUrl: './haupt-seite.component.html',
+  styleUrls: ['./haupt-seite.component.css']
+})
+export class HauptSeiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
